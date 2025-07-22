f2edb9c43558559adcdc02468a938c767ecda354
gkoupp01 <129974219+G1-1ST-H4CK3R@users.noreply.github.com>
2025-07-22 18:11:56 +0300

    Fix Tests

--- test/com/lambdaseq/sectools/core_test.clj
+++ test/com/lambdaseq/sectools/core_test.clj
@@ -3,23 +3,22 @@
             [com.lambdaseq.sectools.core :as sut])
   (:import [java.net ServerSocket]))
 
-(def open-port 7888)                ;; ο “γνωστός ανοιχτός” port
-(def closed-port 7889)              ;; υποθέτουμε ότι είναι ελεύθερος
+(def open-port 7888)                
+(def closed-port 7889)              
 
 (deftest scan-known-localhost-ports
-  ;; Εκκινούμε έναν προσωρινό server στο port 7888
+
   (with-open [srv (ServerSocket. open-port)]
-    ;; 1 . Σαρώνουμε ΜΟΝΟ το 7888 ⇒ πρέπει να είναι :open
     (is (= {:open   [open-port]
             :closed []}
            (sut/scan-ports "localhost" open-port open-port)))
 
-    ;; 2 . Σαρώνουμε και 7888 + 7889
+    
     (let [{:keys [open closed]}
           (sut/scan-ports "127.0.0.1" open-port closed-port 300)]
-      (testing "ο ανοιχτός port ανιχνεύεται σωστά"
+      (testing "Open ports"
         (is (some #{open-port} open)))
-      (testing "ο κλειστός port ανιχνεύεται σωστά"
+      (testing "Closed ports"
         (is (some #{closed-port} closed))))))
 
 
@@ -28,13 +27,13 @@
   "Start a temporary ServerSocket listening on an OS-assigned port and
    return a vector [socket port-number].  Caller must close the socket."
   []
-  (let [socket (ServerSocket. 0)]            ; 0 → pick any free port
+  (let [socket (ServerSocket. 0)]            
     [socket (.getLocalPort socket)]))
 
 (deftest scan-ports-detects-open-and-closed
-  ;; Spin up a dummy server so we control which port is open
+  
   (let [[srv open-port] (free-listening-port)
-        closed-port     (inc open-port)]     ; assume next port isn’t in use
+        closed-port     (inc open-port)]     
     (try
       (let [{:keys [open closed]}
             (sut/scan-ports "127.0.0.1" open-port closed-port 300)]
@@ -42,13 +41,13 @@
           (is (some #{open-port} open)))
         (testing "non-listening port is reported closed"
           (is (some #{closed-port} closed))))
-      (finally (.close srv)))))              ; always shut down the server socket
+      (finally (.close srv)))))             
 
 (deftest scan-ports-default-timeout
-  ;; Same idea, but exercising the 3-arity (default-timeout) version
+
   (let [[srv port] (free-listening-port)]
     (try
       (is (= {:open   [port]
               :closed []}
-             (sut/scan-ports "localhost" port port))) ; uses default 200 ms
+             (sut/scan-ports "localhost" port port)))
       (finally (.close srv)))))
